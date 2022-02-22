fba9b14d8f61b46fe080ccac05682fd7ef926c1f
MonickHelenn <70335517+MonickHelenn@users.noreply.github.com>
2022-02-22 18:46:11 -0300

    Add files via upload

---
+++ BootcampJS/js/main.js
@@ -0,0 +1,18 @@
+/*
+var nome = "Monick";
+var idade = 29;
+var idade2 = 10;
+var frase = "Japão é o melhor time do mundo";
+//alert(nome + " tem " + idade + " anos");
+//alert(idade + idade2);
+console.log(frase.replace("Japão", "Brasil"));
+//alert(frase.replace("Japão", "Brasil")); //Troca Japão por Brasil
+//alert(frase.toUpperCase("Japão", "Brasil")); //Tudo maiusculo
+alert(frase.toLowerCase("Japão", "Brasil")); //jogou tudo pra cima
+*/
+
+//aula2
+
+var lista = ["maça", "pera", "uva"];
+alert(lista[1]);
+console.log(lista[1]);
